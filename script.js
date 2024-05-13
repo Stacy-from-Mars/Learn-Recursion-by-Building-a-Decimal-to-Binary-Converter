d159876d1cf8fbfc7c546507c9642c8e59a313a6
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-05-13 23:15:21 +0300

    step 9

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const checkUserInput = () => {
-    console.log(numberInput.value);
+    if (numberInput.value === "") {
+
+    }
 }
 
 convertBtn.addEventListener("click", checkUserInput);
