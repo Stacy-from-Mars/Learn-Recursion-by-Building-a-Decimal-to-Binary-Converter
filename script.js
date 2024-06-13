f8cfd3383cff93428af9a6fc48f10df7a274765f
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-06-13 23:55:12 +0300

    step 90

--- script.js
+++ script.js
@@ -32,6 +32,9 @@ const decimalToBinary = (input) => {
 
 const showAnimation = () => {
     result.innerText = "Call Stack Animation";
+    animationData.forEach(obj => {
+
+    })
 }
 
 const checkUserInput = () => {
