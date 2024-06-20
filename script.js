8b94c6b16e94e4744f06402f0f131d512599d85b
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-06-20 23:44:04 +0300

    step 102

--- script.js
+++ script.js
@@ -8,7 +8,10 @@ const animationData = [
     {
         inputVal: 5,
         marginTop: 300,
-        addElDelay: 1000
+        addElDelay: 1000,
+        msg: 'decimalToBinary(5) returns "10" + 1 (5 % 2). Then it pops off the stack.',
+        showMsgDelay: 15000,
+        removeElDelay: 20000
     },
     {
         inputVal: 2,
@@ -22,9 +25,9 @@ const animationData = [
         inputVal: 1,
         marginTop: -200,
         addElDelay: 2000,
+        msg: 'decimalToBinary(1) returns "1" (base case) and gives that value to the stack below. Then it pops off the stack.',
         showMsgDelay: 5000,
-        removeElDelay: 10000,
-        msg: 'decimalToBinary(1) returns "1" (base case) and gives that value to the stack below. Then it pops off the stack.'
+        removeElDelay: 10000
     },
 ];
 
